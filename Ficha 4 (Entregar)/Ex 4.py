ec404a0a9976778826d4ff350526518e306f9c3b
Christopher <christopher.40@outlook.pt>
2018-04-17 14:25:54 +0100

    Ficha 4 (Entregar)

--- Ficha 4 (Entregar)/Ex 4.py
+++ Ficha 4 (Entregar)/Ex 4.py
@@ -1,10 +1,7 @@
-num = int(input('Introduza um numero: '))
-
 def numero(n):
-    if n == -1:
-        return 0
-    elif numero(n - 1):
-        return n
+    if n > 0:
+        numero(n -1)
     print(n)
+num = int(input('Introduza um numero: '))
 
 numero(num)
---
+++ Ficha 5 (Entrehar)/Ex 1.py
@@ -0,0 +1,19 @@
+"""import re
+def isValidEmail(email):
+     if len(email) > 7:
+        if re.match("^.+@([?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email) != None:
+            return True
+        return False
+if isValidEmail("my.email@gmail.com") == True:
+ print("This is a valid email address")
+else:
+ print("This is not a valid email address")
+ """
+print("Registar: ")
+print("")
+print("")
+print("")
+nome = input(print("Introduza o seu nome de utilizador: "))
+password = input(print(Insira a sua password: ))
+email = input(print(Insira o seu email: ))
+identificador = int(input(print(Insira o seu numero de telemovel: )))
